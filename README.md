7afb5397fce56c3b58974d62acb5878407bd256b
ppilla1 <informprashant@gmail.com>
2020-05-18 18:05:55 +0530

    Commit#6 : Added/Configured CSS and Style loader

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 #1# Installing Webpack
+
 npm init -y
 npm install --save-dev webpack webpack-cli
 npm start
+
+#2# Install webpack style & css loaders
+
+npm install --save-dev style-loader css-loader
--- src/app-one/index.js
+++ src/app-one/index.js
@@ -1,8 +1,34 @@
+/* Javascript modules importing */
+import { inputsAreValid } from "./utils/inputs-are-valid.util";
+import { parseInputs } from "./utils/parse-inputs.util";
 import { AlertService } from "./services/alert.service";
 import { ComponentService } from "./services/component.service";
-import { run } from "./app-one";
+
+/* CSS modules importing */
+import "./style.css";
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
+
+const run = (alertService, componentService) => {
+    alertService.hideErrors();
+  
+    componentService.onClick(() => {
+      
+      alertService.hideErrors();
+  
+      const inputs = componentService.getInputs();
+      const parsedInputs = parseInputs(...inputs);
+  
+      if (inputsAreValid(...parsedInputs)) {
+        const [numA, numB] = parsedInputs;
+        componentService.setResult(numA + numB);
+      } else {
+        componentService.setResult("");
+        alertService.handleAdditionError(inputs, parsedInputs);
+      }
+    });
+  
+  };
 
 run(alertService, componentService);
